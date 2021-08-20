3ecb6d37aa092500e5f02429c66ba4dc3e27b6e7
Matthias Füller <matthias.fueller@fhdw.de>
2021-08-20 11:25:11 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@
   const int PWM_INPUT_IRQ_PIN = PCINT0; // Int 0 for pin 2 // attiny
   const int PWM_OUTPUT_PIN = PB4;  // attiny PB3 -> leg 2
   const int PWM_OUTPUT_REVERSE_PIN = PB3;  // attiny PB4 -> leg 3
-  const float PWM_FREQUENCY = 504; /* in Hz */
-  const float PWM_RESOLUTION = 8;  /* in bit */
-  const int LED_PIN = PB1;
+  // const float PWM_FREQUENCY = 504; /* in Hz */
+  // const float PWM_RESOLUTION = 8;  /* in bit */
+  // const int LED_PIN = PB1;
 // #define DEBUG
 #elif defined(ARDUINO_TEENSY31)
 #define _TEENSY_
   const int PWM_INPUT_PIN = 14; // teensy 3.2
   const int PWM_INPUT_IRQ_PIN = digitalPinToInterrupt(PWM_INPUT_PIN); // teensy
   const int PWM_OUTPUT_PIN = 10;
-  const float PWM_FREQUENCY = 488; /* in Hz */
-  const float PWM_RESOLUTION = 8;  /* in bit */
+  // const float PWM_FREQUENCY = 488; /* in Hz */
+  // const float PWM_RESOLUTION = 8;  /* in bit */
   // const int LED_PIN = LED_BUILTIN;
 // #define DEBUG
 #else // other platforms
@@ -25,39 +25,38 @@
   const int PWM_INPUT_IRQ_PIN = digitalPinToInterrupt(PWM_INPUT_PIN); // teensy
   const int PWM_OUTPUT_PIN = 10;
   const int PWM_OUTPUT_REVERSE_PIN = PB3;  // attiny PB4 -> leg 3
-  const float PWM_FREQUENCY = 488; /* in Hz */
-  const float PWM_RESOLUTION = 8;  /* in bit */
+  // const float PWM_FREQUENCY = 488; /* in Hz */
+  // const float PWM_RESOLUTION = 8;  /* in bit */
   // const int LED_PIN = LED_BUILTIN;
 // #define DEBUG
 #endif
 
 // valid PWM range definition
-const int PWM_LOW = 1100;    /* in us */
+const int PWM_LOWER_BOUND = 1100;    /* in us */
 const int PWM_CENTER = 1500; /* in us */
-const int PWM_HIGH = 1900;   /* in us */
+const int PWM_UPPER_BOUND = 1900;   /* in us */
 
 // define hysteresis for center value
-const int PWM_CTL_HYSTERESIS = 50; /* in us */
+const int PWM_CTL_HYSTERESIS = 100; /* in us */
 
 // delay on each iteration
-const int LOOP_DELAY_IN_MS = 20;      /* in ms */
-const int SHORT_PRESS_TIME_IN_MS = 200; /* in ms */
+const int LOOP_DELAY_IN_MS = 200;      /* in ms */
+const int SHORT_PRESS_TIME_IN_MS = 50; /* in ms */
 const int LONG_PRESS_TIME_IN_MS = 1000; /* in ms */
 // single step pwm delta
 const int PWM_STEPSIZE = 50; /* in us */
 
-// pwm output
-int pwm_out_duty_in_us = PWM_CENTER; /* in us */
-long mode_last_changed = 0;
-long step_last_changed = 0;
+// state variables
+int pwm_output_pulse_width_in_us = PWM_CENTER; /* in us */
+// long mode_last_changed_in_ms = 0;
+long step_last_changed_in_ms = 0;
 
 // last pwm duty time
-volatile unsigned long pwm_input_duty_in_us = 0;
-volatile unsigned long pwm_start_time = 0;
+volatile unsigned long pwm_input_pulse_width_in_us = 0;
+volatile unsigned long pwm_input_pulse_start_in_us = 0;
 
-unsigned long mode_changed_time = 0;
+unsigned long pwm_input_mode_zero_timestamp_in_ms = 0;
 const int MODE_CHANGE_TIMEDELTA_IN_MS = 100;
-unsigned long last_edge_time = 0;
 
 #ifdef DEBUG
 #if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__)
@@ -81,10 +80,15 @@ unsigned long last_edge_time = 0;
 void handle_pwm_input_interrupt() {
 
   if (digitalRead(PWM_INPUT_PIN)) {
-    pwm_start_time = micros();
-    last_edge_time = millis();
+    pwm_input_pulse_start_in_us = micros();
   } else {
-    pwm_input_duty_in_us = micros() - pwm_start_time;
+    pwm_input_pulse_width_in_us = micros() - pwm_input_pulse_start_in_us;
+
+    // handle overflow after 70min
+    if (pwm_input_pulse_width_in_us < 0)
+    {
+      pwm_input_pulse_width_in_us = 0;
+    }
   }
 }
 
@@ -106,10 +110,11 @@ void setup() {
   pinMode(PWM_OUTPUT_PIN, OUTPUT);
   pinMode(PWM_OUTPUT_REVERSE_PIN, OUTPUT);
 
-  mode_last_changed = millis();
+  pwm_input_mode_zero_timestamp_in_ms = millis();
 #ifdef _ARDUINO_
   attachInterrupt(PWM_INPUT_IRQ_PIN, handle_pwm_input_interrupt, CHANGE);
-#elif defined _ATTINY_
+#elif defined _ATTINY_ // register interrupt for attiny mcus
+
   cli();
 
   GIMSK |= (1 << PCIE);
@@ -122,22 +127,15 @@ void setup() {
 }
 
 // return the pwm value based on the irq handler and reset it to 0
-int get_pwm_duty_time_in_us_and_reset() {
+int get_pwm_duty_time_in_us() {
 
   // reset when last edge is long ago...
-  if (last_edge_time + 1000 < millis())
+  if (pwm_input_pulse_start_in_us + 1000L*1000L < micros())
   {
     // pwm_out_duty_in_us = 0;
   }
 
-  if (pwm_input_duty_in_us > 0)
-  {
-    DEBUG_PRINT("Last time ");
-    DEBUG_PRINTLN(pwm_input_duty_in_us);
-    return pwm_input_duty_in_us;
-  }
-
-  return 0;
+  return pwm_input_pulse_width_in_us;
 }
 
 
@@ -149,18 +147,17 @@ int sign(int x) {
   return 0;
 }
 
-
 void do_soft_pwm(int pin, int high_time_in_us)
 {
-  unsigned long start = micros();
+  unsigned long start_in_us = micros();
 
-  if (high_time_in_us < PWM_LOW || high_time_in_us > PWM_HIGH)
+  if (high_time_in_us < PWM_LOWER_BOUND || high_time_in_us > PWM_UPPER_BOUND)
   {
     return;
   }
 
   digitalWrite(pin, HIGH);
-  while(start + high_time_in_us > micros())
+  while(start_in_us + high_time_in_us > micros())
   {
     // do nothing
   }
@@ -169,76 +166,71 @@ void do_soft_pwm(int pin, int high_time_in_us)
 
 // return [-1,0,1] depending on the pwm input value
 int current_input_mode() {
-  int pwm_in_us = get_pwm_duty_time_in_us_and_reset();
+  int pwm_in_us = get_pwm_duty_time_in_us();
 
-  int pwm_deflection = pwm_in_us - PWM_CENTER;
+  int pwm_control_offset_in_us = pwm_in_us - PWM_CENTER;
 
-  if (abs(pwm_deflection) < PWM_CTL_HYSTERESIS) {
+  if (abs(pwm_control_offset_in_us) < PWM_CTL_HYSTERESIS) {
     return 0;
   }
 
-  if (pwm_deflection > 0) {
+  if (pwm_control_offset_in_us > 0) {
     return 1;
   }
 
   return -1;
 }
 
-void update_pwm_output(int pwm)
+void update_pwm_output(int pwm_width_in_us)
 {
   DEBUG_PRINT("pwm: ");
-  DEBUG_PRINTLN(pwm);
+  DEBUG_PRINTLN(pwm_width_in_us);
 
-  int pwm_r = int(pwm / PWM_STEPSIZE)*PWM_STEPSIZE;
+  int pwm_width_rounded_in_us = int(pwm_width_in_us / PWM_STEPSIZE) * PWM_STEPSIZE;
 
   // pwm_r = 1300;
 
-  do_soft_pwm(PWM_OUTPUT_PIN, pwm_r);
+  do_soft_pwm(PWM_OUTPUT_PIN, pwm_width_rounded_in_us);
   // calculate the reverse pwm_r value
-  int delta_pwm = pwm_r - PWM_LOW;
-  int pwm_inv = PWM_HIGH - delta_pwm;
+  int pwm_width_reversed_in_us = PWM_UPPER_BOUND - (pwm_width_rounded_in_us - PWM_LOWER_BOUND);
 
-  do_soft_pwm(PWM_OUTPUT_REVERSE_PIN, pwm_inv);
+  do_soft_pwm(PWM_OUTPUT_REVERSE_PIN, pwm_width_reversed_in_us);
 
 }
 
 
 void loop() {
 
-  int mode_in = current_input_mode();
+  int input_mode = current_input_mode();
 
   DEBUG_PRINT("mode_in: ");
-  DEBUG_PRINTLN(mode_in);
+  DEBUG_PRINTLN(input_mode);
 
-  if (mode_in != 0) {
-
-  } else {
-    mode_changed_time = millis();
-    mode_last_changed = millis();
+  if (input_mode == 0) {
+    pwm_input_mode_zero_timestamp_in_ms = millis();
   }
 
-  if (mode_changed_time + MODE_CHANGE_TIMEDELTA_IN_MS < millis() ) {
-    long delta_t = millis() - mode_last_changed;
-    long delta_t_step = millis() - step_last_changed;
+  long input_mode_nonzero_timedelta_in_ms = millis() - pwm_input_mode_zero_timestamp_in_ms;
 
-    int pwm_offset = mode_in * PWM_STEPSIZE;
+  if (input_mode_nonzero_timedelta_in_ms > MODE_CHANGE_TIMEDELTA_IN_MS) {
+
+    int pwm_offset_in_us = input_mode * PWM_STEPSIZE;
     // int pwm_offset = mode_in * (0.1 * delta_t);
     DEBUG_PRINT("pwm_offset: ");
-    DEBUG_PRINTLN(pwm_offset);
+    DEBUG_PRINTLN(pwm_offset_in_us);
 
-    if (delta_t_step > SHORT_PRESS_TIME_IN_MS) {
-      pwm_out_duty_in_us =
-          min(max(pwm_out_duty_in_us + pwm_offset, PWM_LOW), PWM_HIGH);
-      step_last_changed = millis();
+    if (input_mode_nonzero_timedelta_in_ms > SHORT_PRESS_TIME_IN_MS) {
+      pwm_output_pulse_width_in_us =
+          min(max(pwm_output_pulse_width_in_us + pwm_offset_in_us, PWM_LOWER_BOUND), PWM_UPPER_BOUND);
+      step_last_changed_in_ms = millis();
     }
 
-    // long press
-    if (delta_t > LONG_PRESS_TIME_IN_MS) {
-      if (mode_in < 0) {
-        pwm_out_duty_in_us = PWM_LOW;
-
+    // long press -> output to upper or lower bounds
+    if (input_mode_nonzero_timedelta_in_ms > LONG_PRESS_TIME_IN_MS) {
+      if (input_mode < 0) {
+        pwm_output_pulse_width_in_us = PWM_LOWER_BOUND;
       } else {
-        pwm_out_duty_in_us = PWM_HIGH;
+        pwm_output_pulse_width_in_us = PWM_UPPER_BOUND;
       }
     }
 
@@ -247,7 +239,7 @@ void loop() {
 
   }
 
-  update_pwm_output(pwm_out_duty_in_us);
+  update_pwm_output(pwm_output_pulse_width_in_us);
 
   DEBUG_PRINTLN("####");
   delay(LOOP_DELAY_IN_MS);
